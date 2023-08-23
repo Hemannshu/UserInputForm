a921d9097073da2e5e179197711c21b2ddfb6f9f
Himanshu Sharma <141823832+Hemannshu@users.noreply.github.com>
2023-08-23 19:58:54 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.querySelector("form");
+    
+    form.addEventListener("submit", function(event) {
+        event.preventDefault();
+
+        const name = form.querySelector("input[type='text']").value;
+        const email = form.querySelector("input[type='email']").value;
+        const phoneNumber = form.querySelector("input[type='number']").value;
+        const gender = form.querySelector("input[name='gender']:checked").value;
+
+        const userDetails = {
+            name: name,
+            email: email,
+            phoneNumber: phoneNumber,
+            gender: gender
+        };
+
+        // Convert the object to a JSON string
+        const userDetailsJSON = JSON.stringify(userDetails);
+
+        // Save the JSON string in local storage
+        localStorage.setItem("userDetails", userDetailsJSON);
+
+        alert("User details saved to local storage.");
+		location.reload();
+    });
+});
